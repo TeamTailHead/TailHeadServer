2deb0f3552add1e81b638ac7411be819a3fb4b9e
csb9427 <csb9427@naver.com>
2022-11-08 19:22:55 +0900

    fix: 객체 생성 추가

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import { NodeSocketServer, StringServerCommunicator } from "@tailhead/communicator";
 
 import Game from "./Game";
+import PlayerService from "./PlayerService";
 
 const PORT = 5055;
 
 async function main() {
   const server = new NodeSocketServer();
   const communicator = new StringServerCommunicator(server);
-  const game = new Game(communicator, {
+  const playerService = new PlayerService(communicator);
+  const game = new Game(communicator, playerService, {
     isWordExists(word) {
       return word.length > 0;
     },
