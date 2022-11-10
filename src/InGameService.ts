65776cc411ce62e14d85d3687db1c87c0d8d2035
csb9427 <csb9427@naver.com>
2022-11-10 18:41:51 +0900

    feat: InGameService 기본 채팅 구현

---
+++ src/InGameService.ts
@@ -0,0 +1,16 @@
+import { ServerCommunicator } from "@tailhead/communicator";
+
+import PlayerService from "./PlayerService";
+
+export default class InGameService {
+  constructor(private communicator: ServerCommunicator, private playerService: PlayerService) {}
+
+  playerChat(playerId: string, content: string) {
+    const sendPlayer = this.playerService.getPlayer(playerId);
+    if (!sendPlayer) {
+      throw new Error("유령이다 유령");
+    }
+
+    this.communicator.sendAll("playerChat", { playerId, nickname: sendPlayer.nickname, content });
+  }
+}
