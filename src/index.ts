d9e30868ab59519c57548cba80ba25b8bfe065dd
Tekiter <tekiter8@gmail.com>
2022-11-15 20:49:13 +0900

    feat: communicator 버전 변경

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { NodeSocketServer, StringServerCommunicator } from "@tailhead/communicator";
+import { BinaryServerCommunicator, NodeSocketServer } from "@tailhead/communicator";
 
 import Game from "./Game";
 import InGameService from "./InGameService";
@@ -11,7 +11,7 @@ const PORT = 5055;
 async function main() {
   const logger = new ConsoleLoggerService();
   const server = new NodeSocketServer();
-  const communicator = new StringServerCommunicator(server);
+  const communicator = new BinaryServerCommunicator(server);
   const playerService = new PlayerService(communicator, logger);
   const lobbyService = new LobbyService(communicator, playerService);
   const inGameService = new InGameService(communicator, playerService);
